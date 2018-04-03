00353f579e65a9db1a0656035f739d9a4be34559
jerry.chen <jerry.chen@astasys.com>
2018-04-03 11:58:10 +0800

    [url-plugin]. add 输出json到前台的模板controller

---
+++ src/main/java/org/remix/ssh/controller/project/urlplugin/UrlNotePlugin.java
@@ -0,0 +1,35 @@
+package org.remix.ssh.controller.project.urlplugin;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
+@RequestMapping("/plugin")
+public class UrlNotePlugin {
+    @RequestMapping("/url")
+    public void test(HttpServletRequest request, HttpServletResponse response){
+        String json = "{'code': '成功'}";
+        returnJson(request, response, json);
+    }
+
+    public void returnJson(HttpServletRequest request, HttpServletResponse response, String json){
+        try{
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter out = response.getWriter();
+            out.print(json);
+            out.flush();
+            out.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
