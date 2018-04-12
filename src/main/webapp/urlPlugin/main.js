e837c0733e526a0e890ffe488b6249b222a6f980
jerry.chen <jerry.chen@astasys.com>
2018-04-12 09:54:40 +0800

    删除注释

--- src/main/webapp/urlPlugin/main.js
+++ src/main/webapp/urlPlugin/main.js
@@ -17,27 +17,6 @@ function httpRequest(urlToSave, title, folder, callback) {
     xhr.send();
 }
 
-// render
-/*function showResult(result) {
-	var table = '<table><thead><tr><th>股票</th><th>最新价</th><th>涨跌幅</th></tr></thead><tbody>';
-	var arr = result.split(';').slice(0, -1);
-	arr.forEach(function(item){
-		var itemArr = item.split('=')[1].split('"')[1].split(','),
-			name = itemArr[0];
-			curr = Number(itemArr[3]).toFixed(2);
-			yest = Number(itemArr[2]).toFixed(2);
-			range = (((curr - yest) / yest) * 100).toFixed(2);
-		
-		table += '<tr>';
-		table += '<td>' + name + '</td>';
-		table += '<td>' + curr + '</td>';
-		table += '<td>' + range + '</td>';
-		table += '</tr>';
-	})
-	table += '</tbody></table>';
-	document.getElementById('stock').innerHTML = table;
-}*/
-
 function showResult(result) {
     var json = JSON.parse(result);
     if(json.result == 'suc') {
@@ -49,36 +28,6 @@ function showResult(result) {
     }
 }
 
-// add
-/*document.getElementById('addBtn').onclick = function() {
-	var add = document.getElementById('add'),
-		addBtn = document.getElementById('addBtn');
-		
-	var input = document.createElement('input');
-	input.type = 'text';
-	input.id = 'newStock';
-
-	var saveBtn = document.createElement('input');
-	saveBtn.type = 'button';
-	saveBtn.id = 'saveBtn';
-	saveBtn.value = 'save';
-
-	add.removeChild(addBtn);
-	add.appendChild(input);
-	add.appendChild(saveBtn);
-
-	document.getElementById('saveBtn').onclick = function() {
-		var newStock = document.getElementById('newStock').value;
-		var stocks = localStorage.stocks && localStorage.stocks.split(',') || ['sh000001'];
-		stocks.push(newStock);
-		localStorage.stocks = stocks;
-		httpRequest(showResult);
-
-		add.removeChild(input);
-		add.removeChild(saveBtn);
-		add.appendChild(addBtn);
-	}
-}*/
 document.getElementById('saveUrlBtn').onclick = function() {
     var urlToSave = window.location.href;
     var title = document.title;
@@ -91,12 +40,7 @@ document.getElementById('saveUrlBtn').onclick = function() {
     httpRequest(urlToSave, title, folder, showResult);
 }
 
-
-
-// httpRequest(showResult);
-var domain = document.origin;
-
-var url = domain+'/plugin/folder/all';
+var url = document.origin+'/plugin/folder/all';
 $.ajax({
     url : url,
     type : "post",
@@ -176,12 +120,15 @@ function getTree() {
 
     return tree;
 }
+/*
 $('#tree').treeview({
     data: getTree(),
     onNodeSelected: function (event, data) {
         alert("event");
     }
 });
+*/
+
 
 
 
