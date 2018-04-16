5f43b24f815b850cdb888da93b11e327880ef892
jerry.chen <jerry.chen@astasys.com>
2018-04-16 18:53:32 +0800

    [demo] add dtree

---
+++ src/main/webapp/dtree/js/dtree.js
@@ -0,0 +1,82 @@
+$(function () {
+    $(".dtree").height($(window).height());
+    $(document).on('click','.dtree h2',function(){
+        //$(".dtree h2").click(function(){
+        /*		if($(this).hasClass("up")){
+                    $(this).siblings("ol").slideUp("fast").removeClass("up");
+                }else{
+                    $(this).siblings("ol").slideDown('fast').addClass("up");
+                }*/
+        if($(this).hasClass("up")){
+            $(this).siblings("ol").slideUp("fast");
+            $(this).removeClass("up");
+        }else{
+            $(".dtree ol").slideUp("fast");
+            $(".dtree h2").removeClass("up");
+            $(this).siblings("ol").slideDown('fast');
+            $(this).addClass("up");
+        }
+    });
+    $("#openAll").click(function(){
+        $(".dtree ol").show();
+        $(".dtree h2").addClass("up");
+    });
+    $("#closeAll").click(function(){
+        $(".dtree ol").hide();
+        $(".dtree h2").removeClass("up");
+    });
+
+    /*$(".dtree li").on('click','a', function() {
+        $(".dtree li a.up").removeClass("up");
+        $(this).addClass("up");
+        //$(this).attr("href","javascript:void(0)");
+        //var thisHref = $(this).attr(href);
+    });*/
+
+
+    $.ajax({
+        url: 'js/data.txt',
+        dataType: 'json'
+    }).done(function (data) {
+        var status = $('#selection'),
+            countries = $.map(data, function (value) {
+                return value;
+            });
+
+        $('#query').autocomplete({
+            width:188,
+            lookup: countries,
+            onSelect: function (suggestion) {
+                //status.html('You selected: ' + suggestion);
+                var url = suggestion+".html";
+                $("#menu li a[href='"+url+"']")[0].click();
+            }
+        });
+    });
+    $('#query').keydown(function(e){
+        if(e.keyCode==13){
+            var soval = $("#query").val();
+            if(soval){
+                var url = soval+".html";
+                $("#menu li a[href='"+url+"']")[0].click();
+            }
+        }
+    })
+    $("#butsoso").click(function(){
+        var soval = $("#query").val();
+        if(soval){
+            var url = soval+".html";
+            $("#menu li a[href='"+url+"']")[0].click();
+        }
+    });
+});
+
+
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?b3a3fc356d0af38b811a0ef8d50716b8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
