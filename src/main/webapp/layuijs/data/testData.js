bcb20b4ffd3f6851118a559f3ae444a347a50f7d
jerry.chen <jerry.chen@astasys.com>
2018-06-15 16:04:03 +0800

    add test data

---
+++ src/main/webapp/layuijs/data/testData.js
@@ -0,0 +1,159 @@
+var layimTestInit = {
+    "mine": {
+        "username": "纸飞机"
+        ,"id": "100000"
+        ,"status": "online"
+        ,"sign": "在深邃的编码世界，做一枚轻盈的纸飞机"
+        ,"avatar": "http://cdn.firstlinkapp.com/upload/2016_6/1465575923433_33812.jpg"
+    }
+    ,"friend": [{
+        "groupname": "前端码屌"
+        ,"id": 1
+        ,"online": 2
+        ,"list": [{
+            "username": "贤心"
+            ,"id": "100001"
+            ,"avatar": "http://tp1.sinaimg.cn/1571889140/180/40030060651/1"
+            ,"sign": "这些都是测试数据，实际使用请严格按照该格式返回"
+        },{
+            "username": "Z_子晴"
+            ,"id": "108101"
+            ,"avatar": "http://tva3.sinaimg.cn/crop.0.0.512.512.180/8693225ajw8f2rt20ptykj20e80e8weu.jpg"
+            ,"sign": "微电商达人"
+        },{
+            "username": "Lemon_CC"
+            ,"id": "102101"
+            ,"avatar": "http://tp2.sinaimg.cn/1833062053/180/5643591594/0"
+            ,"sign": ""
+        },{
+            "username": "马小云"
+            ,"id": "168168"
+            ,"avatar": "http://tp4.sinaimg.cn/2145291155/180/5601307179/1"
+            ,"sign": "让天下没有难写的代码"
+            ,"status": "offline"
+        },{
+            "username": "徐小峥"
+            ,"id": "666666"
+            ,"avatar": "http://tp2.sinaimg.cn/1783286485/180/5677568891/1"
+            ,"sign": "代码在囧途，也要写到底"
+        }]
+    },{
+        "groupname": "网红"
+        ,"id": 2
+        ,"online": 3
+        ,"list": [{
+            "username": "罗玉凤"
+            ,"id": "121286"
+            ,"avatar": "http://tp1.sinaimg.cn/1241679004/180/5743814375/0"
+            ,"sign": "在自己实力不济的时候，不要去相信什么媒体和记者。他们不是善良的人，有时候候他们的采访对当事人而言就是陷阱"
+        },{
+            "username": "长泽梓Azusa"
+            ,"id": "100001222"
+            ,"sign": "我是日本女艺人长泽あずさ"
+            ,"avatar": "http://tva1.sinaimg.cn/crop.0.0.180.180.180/86b15b6cjw1e8qgp5bmzyj2050050aa8.jpg"
+        },{
+            "username": "大鱼_MsYuyu"
+            ,"id": "12123454"
+            ,"avatar": "http://tp1.sinaimg.cn/5286730964/50/5745125631/0"
+            ,"sign": "我瘋了！這也太準了吧  超級笑點低"
+        },{
+            "username": "谢楠"
+            ,"id": "10034001"
+            ,"avatar": "http://tp4.sinaimg.cn/1665074831/180/5617130952/0"
+            ,"sign": ""
+        },{
+            "username": "柏雪近在它香"
+            ,"id": "3435343"
+            ,"avatar": "http://tp2.sinaimg.cn/2518326245/180/5636099025/0"
+            ,"sign": ""
+        }]
+    },{
+        "groupname": "我心中的女神"
+        ,"id": 3
+        ,"online": 1
+        ,"list": [{
+            "username": "林心如"
+            ,"id": "76543"
+            ,"avatar": "http://tp3.sinaimg.cn/1223762662/180/5741707953/0"
+            ,"sign": "我爱贤心"
+        },{
+            "username": "佟丽娅"
+            ,"id": "4803920"
+            ,"avatar": "http://tp4.sinaimg.cn/1345566427/180/5730976522/0"
+            ,"sign": "我也爱贤心吖吖啊"
+        }]
+    }]
+    ,"group": [{
+        "groupname": "前端群"
+        ,"id": "101"
+        ,"avatar": "http://tp2.sinaimg.cn/2211874245/180/40050524279/0"
+    },{
+        "groupname": "Fly社区官方群"
+        ,"id": "102"
+        ,"avatar": "http://tp2.sinaimg.cn/5488749285/50/5719808192/1"
+    }]
+};
+
+var layimTestMembers = {
+    "list": [{
+        "username": "贤心"
+        ,"id": "100001"
+        ,"avatar": "//tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"
+        ,"sign": "这些都是测试数据，实际使用请严格按照该格式返回"
+    },{
+        "username": "Z_子晴"
+        ,"id": "108101"
+        ,"avatar": "//tva1.sinaimg.cn/crop.0.23.1242.1242.180/8693225ajw8fbimjimpjwj20yi0zs77l.jpg"
+        ,"sign": "微电商达人"
+    },{
+        "username": "Lemon_CC"
+        ,"id": "102101"
+        ,"avatar": "//tva4.sinaimg.cn/crop.0.0.180.180.180/6d424ea5jw1e8qgp5bmzyj2050050aa8.jpg"
+        ,"sign": ""
+    },{
+        "username": "马小云"
+        ,"id": "168168"
+        ,"avatar": "//tva1.sinaimg.cn/crop.0.0.180.180.180/7fde8b93jw1e8qgp5bmzyj2050050aa8.jpg"
+        ,"sign": "让天下没有难写的代码"
+    },{
+        "username": "徐小峥"
+        ,"id": "666666"
+        ,"avatar": "//tva1.sinaimg.cn/crop.0.0.512.512.180/6a4acad5jw8eqi6yaholjj20e80e8t9f.jpg"
+        ,"sign": "代码在囧途，也要写到底"
+    },{
+        "username": "罗玉凤"
+        ,"id": "121286"
+        ,"avatar": "//tva4.sinaimg.cn/crop.0.0.640.640.180/4a02849cjw8fc8vn18vktj20hs0hs75v.jpg"
+        ,"sign": "在自己实力不济的时候，不要去相信什么媒体和记者。他们不是善良的人，有时候候他们的采访对当事人而言就是陷阱"
+    },{
+        "username": "刘涛tamia"
+        ,"id": "100001222"
+        ,"avatar": "//tva4.sinaimg.cn/crop.0.1.1125.1125.180/475bb144jw8f9nwebnuhkj20v90vbwh9.jpg"
+        ,"sign": "如约而至，不负姊妹欢乐颂"
+    },{
+        "username": "大鱼_MsYuyu"
+        ,"id": "12123454"
+        ,"avatar": "//tva2.sinaimg.cn/crop.0.0.512.512.180/005LMAegjw8f2bp9qg4mrj30e80e8dg5.jpg"
+        ,"sign": "我瘋了！這也太準了吧  超級笑點低"
+    },{
+        "username": "谢楠"
+        ,"id": "10034001"
+        ,"avatar": "//tva2.sinaimg.cn/crop.1.0.747.747.180/633f068fjw8f9h040n951j20ku0kr74t.jpg"
+        ,"sign": ""
+    },{
+        "username": "柏雪近在它香"
+        ,"id": "3435343"
+        ,"avatar": "//tva2.sinaimg.cn/crop.0.8.751.751.180/961a9be5jw8fczq7q98i7j20kv0lcwfn.jpg"
+        ,"sign": ""
+    },{
+        "username": "林心如"
+        ,"id": "76543"
+        ,"avatar": "//tva3.sinaimg.cn/crop.0.0.512.512.180/48f122e6jw8fcmi072lkyj20e80e8t9i.jpg"
+        ,"sign": "我爱贤心"
+    },{
+        "username": "佟丽娅"
+        ,"id": "4803920"
+        ,"avatar": "//tva3.sinaimg.cn/crop.0.0.750.750.180/5033b6dbjw8etqysyifpkj20ku0kuwfw.jpg"
+        ,"sign": "我也爱贤心吖吖啊"
+    }]
+};
