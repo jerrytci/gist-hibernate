5e47abf503f9ad74732d14345136f8d7f39d76fe
jerry.chen <jerry.chen@astasys.com>
2018-06-19 16:18:41 +0800

    多个bean: @resource和@qualifier 注解

---
+++ src/main/java/practice/hibernate52/dao/impl/FolderDaoImpl2.java
@@ -0,0 +1,23 @@
+package practice.hibernate52.dao.impl;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import practice.hibernate52.dao.FolderDao;
+
+import java.util.List;
+
+
+@Repository
+public class FolderDaoImpl2 implements FolderDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public <T> List<T> get(Class<T> clazz) {
+        String sql = "from "+clazz.getTypeName();
+        Query<T> query = sessionFactory.getCurrentSession().createQuery(sql, clazz);
+        return query.list();
+    }
+}
--- src/main/java/practice/hibernate52/service/impl/FolderServiceImpl2.java
+++ src/main/java/practice/hibernate52/service/impl/FolderServiceImpl2.java
@@ -1,17 +1,24 @@
 package practice.hibernate52.service.impl;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import practice.hibernate52.dao.FolderDao;
 import practice.hibernate52.service.FolderService;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Transactional
 @Service
 public class FolderServiceImpl2 implements FolderService{
 
+    @Resource
+    @Qualifier("folderDaoImpl2")
+    private FolderDao folderDao;
+
     @Override
     public <T> List<T> get(Class<T> tClass) {
-        return null;
+        return folderDao.get(tClass);
     }
 }
